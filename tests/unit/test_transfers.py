671f5ac57fa288e0b3d55d8fed791c28f60a8f08
Mateusz Nitkowski <mateusz.nitkowski@gmail.com>
2025-11-13 15:28:05 +0100

    Feature 12 and variable types

--- tests/unit/test_transfers.py
+++ tests/unit/test_transfers.py
@@ -4,31 +4,31 @@ from src.BusinessAccount import BusinessAccount
 
 class TestPersonalTransfers:
     def test_normal_transfer_enough(self):
-        acc = PersonalAccount("Ewa", "Karp", "12345678901", "PROM_420")
+        acc: PersonalAccount = PersonalAccount("Ewa", "Karp", "12345678901", "PROM_420")
         assert acc.normalTransfer(20) is True
         assert acc.history == [-20]
 
     def test_normal_transfer_not_enough(self):
-        acc = PersonalAccount("Pawel", "Malinowski", "12345678901", "PROM_4200")
+        acc: PersonalAccount = PersonalAccount("Pawel", "Malinowski", "12345678901", "PROM_4200")
         assert acc.normalTransfer(20) is False
 
 
     def test_express_transfer_enough(self):
-        acc = PersonalAccount("Tomasz", "Buczkowicz", "12345678901", "PROM_420")
+        acc: PersonalAccount = PersonalAccount("Tomasz", "Buczkowicz", "12345678901", "PROM_420")
         assert acc.expressTransfer(50) is True
         assert acc.balance == -1
         assert acc.history == [-50, -1]
 
     def test_express_transfer_not_enough(self):
-        acc = PersonalAccount("Maciej", "Niedzwiedz", "12345678901", "PROM_420")
+        acc: PersonalAccount = PersonalAccount("Maciej", "Niedzwiedz", "12345678901", "PROM_420")
         assert acc.expressTransfer(51) is False
 
     def test_express_transfer_not_enough_2(self):
-        acc = PersonalAccount("Pawel", "Sobolewski", "12345678901", "PROM_4200")
+        acc: PersonalAccount = PersonalAccount("Pawel", "Sobolewski", "12345678901", "PROM_4200")
         assert acc.expressTransfer(50) is False
 
     def test_receive_transfer(self):
-        acc = PersonalAccount("Pawel", "Sobolewski", "12345678901", "PROM_4200")
+        acc: PersonalAccount = PersonalAccount("Pawel", "Sobolewski", "12345678901", "PROM_4200")
         assert acc.receiveTransfer(69)
         assert acc.balance == 69
         assert acc.history == [69]
@@ -36,34 +36,34 @@ class TestPersonalTransfers:
 
 class TestBusinessTransfers:
     def test_normal_transfer_enough(self):
-        acc = BusinessAccount("Drutex sp. z o.o.", "1234567890")
+        acc: BusinessAccount = BusinessAccount("Drutex sp. z o.o.", "1234567890")
         acc.balance = 20
         assert acc.normalTransfer(20) is True
         assert acc.history == [-20]
 
     def test_normal_transfer_not_enough(self):
-        acc = BusinessAccount("Drutex sp. z o.o.", "1234567890")
+        acc: BusinessAccount = BusinessAccount("Drutex sp. z o.o.", "1234567890")
         assert acc.normalTransfer(20) is False
 
 
     def test_express_transfer_enough(self):
-        acc = BusinessAccount("Drutex sp. z o.o.", "1234567890")
+        acc: BusinessAccount = BusinessAccount("Drutex sp. z o.o.", "1234567890")
         acc.balance = 50
         assert acc.expressTransfer(50) is True
         assert acc.balance == -5
         assert acc.history == [-50, -5]
 
     def test_express_transfer_not_enough(self):
-        acc = BusinessAccount("Drutex sp. z o.o.", "1234567890")
+        acc: BusinessAccount = BusinessAccount("Drutex sp. z o.o.", "1234567890")
         acc.balance = 50
         assert acc.expressTransfer(51) is False
 
     def test_express_transfer_not_enough_2(self):
-        acc = BusinessAccount("Drutex sp. z o.o.", "1234567890")
+        acc: BusinessAccount = BusinessAccount("Drutex sp. z o.o.", "1234567890")
         assert acc.expressTransfer(50) is False
 
     def test_receive_transfer(self):
-        acc = PersonalAccount("Pawel", "Sobolewski", "12345678901", "PROM_4200")
+        acc: BusinessAccount = BusinessAccount("Drutex sp. z o.o.", "1234567890")
         assert acc.receiveTransfer(69)
         assert acc.balance == 69
         assert acc.history == [69]
