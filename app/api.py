dac84a182a695010ddf8c31e71af53f85aa9a7a6
mateusz.nitkowski@gmail.com <Jungkookopp@123>
2025-12-18 15:42:51 +0100

    Feature 16-17 and api test

--- app/api.py
+++ app/api.py
@@ -10,6 +10,8 @@ def createAccount():
     if not request.is_json:
         return jsonify({"error": "Invalid input"}), 400
     data: dict = request.get_json()
+    if registry.search(data["pesel"]):
+        return jsonify({"error": "Account already exists"}), 409
     print(f"Create account request: {data}")
     account: pa = pa(data["name"], data["surname"], data["pesel"])
     registry.addAccount(account)
@@ -70,3 +72,45 @@ def deleteAccount(pesel: int):
 
     registry.removeAccount(pesel)
     return jsonify({"message": "Account deleted"}), 200
+
+@app.route("/api/accounts/<pesel>/transfer", methods=['POST'])
+def handleTransfer(pesel):
+    acc: pa = registry.search(pesel)
+
+    if acc is None:
+        return jsonify({"error": "Account not found"}), 404
+
+    if not request.is_json:
+        return jsonify({"error": "Invalid input"}), 400
+
+    data = request.get_json()
+
+    if "amount" not in data or "type" not in data:
+        return jsonify({"error": "Missing data"}), 400
+
+    if data['type'] not in ['express', 'outgoing', 'incoming']:
+        return jsonify({"error": "Invalid transfer type"}), 400
+
+    try:
+        amount = float(data['amount'])
+        if amount <= 0:
+            return jsonify({"error": "Amount must be positive"}), 400
+
+        r = None
+
+        if data['type'] == 'incoming':
+            r = acc.receiveTransfer(amount)
+        elif data['type'] == 'outgoing':
+            r = acc.normalTransfer(amount)
+        elif data['type'] == 'express':
+            r = acc.expressTransfer(amount)
+
+    except Exception as e:
+        print(e)
+        return jsonify({"error": "Invalid amount"}), 400
+
+    if r:
+        return jsonify({"message": "Accepted for processing"}), 200
+
+    else:
+        return jsonify({"error": "Insufficient funds"}), 422
